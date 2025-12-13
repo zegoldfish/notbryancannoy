89fae4107f6e1748d2185860389de02c269f5412
Bryan Cannoy <notbryancannoy@gmail.com>
2025-12-13 00:11:12 -0500

    increase max body size limit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "5mb",
+      bodySizeLimit: "10mb",
     },
   },
 };
